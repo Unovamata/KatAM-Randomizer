78e6f0007244d17373c1be76d78f05609e9f1067
Bryan <bryangamer83@gmail.com>
2024-05-23 22:51:17 -0600

    Base Object Structure & Mirrors
    • Created the base object structure to inherit from.
    • Mirror class constructor.

--- Classes.cs
+++ Classes.cs
@@ -6,11 +6,63 @@ using System.Threading.Tasks;
 
 namespace KatAMRandomizerClasses{
     public class Object {
+        int address, hp, copyAbility, palette, x, y, id, behavior, speed;
 
+        public Object(int Address, int HP, int CopyAbility, int Palette,
+                      int X, int Y, int ID, int Behavior, int Speed) {
+            address = Address;
+            hp = HP;
+            copyAbility = CopyAbility;
+            palette = Palette;
+            x = X; 
+            y = Y;
+            id = ID;
+            behavior = Behavior;
+            speed = Speed;
+        }
+
+        public int GetAddress() { return address; }
+        public void SetAddress(int value) { address = value; }
+
+        public int GetHp() { return hp; }
+        public void SetHp(int value) { hp = value; }
+
+        public int GetCopyAbility() { return copyAbility; }
+        public void SetCopyAbility(int value) { copyAbility = value; }
+
+        public int GetPalette() { return palette; }
+        public void SetPalette(int value) { palette = value; }
+
+        public int GetX() { return x; }
+        public void SetX(int value) { x = value; }
+
+        public int GetY() { return y; }
+        public void SetY(int value) { y = value; }
+
+        public int GetId() { return id; }
+        public void SetId(int value) { id = value; }
+
+        public int GetBehavior() { return behavior; }
+        public void SetBehavior(int value) { behavior = value; }
+
+        public int GetSpeed() { return speed; }
+        public void SetSpeed(int value) { speed = value; }
     }
 
-    public class Mirror {
+    public class Mirror : Object{
+        string name;
+        int eightRom, nineRom;
+        string exits, description;
 
+        public Mirror(string Name, int Address, int EightRom, int NineRom, int ID, 
+                      string Exits, string Description, int X, int Y)
+        : base(Address, 0, 0, 0, X, Y, ID, 0, 0) {
+            name = Name;
+            eightRom = EightRom;
+            nineRom = NineRom;
+            exits = Exits;
+            description = Description;
+        }
     }
 
     public class Item {
